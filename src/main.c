f8f74d504000c07fc684618ac3a6e1535d2de77d
Subhan Gadirli <subhan.gadirli.en@student.aztu.edu.az>
2026-04-08 23:40:44 +0400

    flatpak support

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <gtk/gtk.h>
+#ifdef HAVE_APPINDICATOR
 #include <libayatana-appindicator/app-indicator.h>
+#endif
 #include <glib/gi18n.h>
 #include <locale.h>
 
@@ -26,6 +28,7 @@ int main(int argc, char *argv[])
 
     gtk_init(&argc, &argv);
 
+#ifdef HAVE_APPINDICATOR
     AppIndicator *indicator = app_indicator_new(
         "nazar",
         "nazar",
@@ -42,6 +45,24 @@ int main(int argc, char *argv[])
     gtk_widget_show_all(menu);
 
     app_indicator_set_menu(indicator, GTK_MENU(menu));
+#else
+    /* Fallback: show window without tray support */
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), _("Nazar Blocker"));
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 100);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    GtkWidget *label = gtk_label_new(_("May light shield me from envy and harm."));
+    gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
+
+    GtkWidget *quit_btn = gtk_button_new_with_label(_("Exit"));
+    g_signal_connect(quit_btn, "clicked", G_CALLBACK(on_quit), NULL);
+    gtk_box_pack_start(GTK_BOX(vbox), quit_btn, FALSE, FALSE, 0);
+
+    gtk_widget_show_all(window);
+#endif
 
     gtk_main();
     return 0;
